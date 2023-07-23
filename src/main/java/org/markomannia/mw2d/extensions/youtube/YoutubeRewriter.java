87864fa545062ea8d878870f91b5471ac55da391
Ulrich Wolffgang <uwol@users.noreply.github.com>
2023-07-23 13:19:40 +0200

    youtube

--- src/main/java/org/markomannia/mw2d/extensions/youtube/YoutubeRewriter.java
+++ src/main/java/org/markomannia/mw2d/extensions/youtube/YoutubeRewriter.java
@@ -3,7 +3,7 @@ package org.markomannia.mw2d.extensions.youtube;
 public class YoutubeRewriter {
 
 	public static String rewriteYoutubeLinks(final String markdown) {
-		return markdown.replaceAll("\\{\\{#ev:youtube\\|([A-Za-z0-9]+)(\\|[0-9]+)?(\\|(right|left))?\\}\\}",
-				"[YouTube-Video](https://youtu.be/$1)");
+		return markdown.replaceAll("\\{\\{#ev:youtube\\|([A-Za-z0-9-\\\\_]+)(\\|[0-9]+)?(\\|(right|left))?\\}\\}",
+				"[YouTube-Video](https://youtu.be/$1)").replace("\\_", "_");
 	}
 }
--- src/test/java/org/markomannia/mw2d/extensions/youtube/YoutubeRewriterTest.java
+++ src/test/java/org/markomannia/mw2d/extensions/youtube/YoutubeRewriterTest.java
@@ -7,8 +7,20 @@ import org.junit.jupiter.api.Test;
 public class YoutubeRewriterTest {
 
 	@Test
-	public void test() throws Exception {
+	public void test1() throws Exception {
 		assertEquals("some [YouTube-Video](https://youtu.be/dVQOLiwaEgQ) test",
 				YoutubeRewriter.rewriteYoutubeLinks("some {{#ev:youtube|dVQOLiwaEgQ|100|right}} test"));
+	}
+
+	@Test
+	public void test2() throws Exception {
+		assertEquals("some [YouTube-Video](https://youtu.be/5K6F-OznPPM) test",
+				YoutubeRewriter.rewriteYoutubeLinks("some {{#ev:youtube|5K6F-OznPPM|100|right}} test"));
+	}
+
+	@Test
+	public void test3() throws Exception {
+		assertEquals("some [YouTube-Video](https://youtu.be/enFh_JdqMHM) test",
+				YoutubeRewriter.rewriteYoutubeLinks("some {{#ev:youtube|enFh\\_JdqMHM|100|right}} test"));
 	}
 }
